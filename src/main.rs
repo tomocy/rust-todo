7b56883fd36641593f7db709741ac68df43d3e8b
tomocy <tomocy.dev@gmail.com>
2020-05-24 22:48:44 +0900

    update entrypoint
    file::UserRepo is injected into cli::App.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::process;
 use todo::gateway::controller;
 use todo::gateway::controller::cli;
 use todo::gateway::presenter::text;
-use todo::infra::memory;
+use todo::infra::file;
 
 fn main() {
-    let mut repo: Box<dyn todo::UserRepo> = Box::new(memory::UserRepo::new());
+    let mut repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new("./"));
     let renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
     let mut app = cli::App::new(&mut repo, &renderer);
     if let Err(err) = app.run() {
