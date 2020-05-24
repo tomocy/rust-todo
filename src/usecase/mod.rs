9945d22a6ceef1742a9c861cd14659dac97b7068
tomocy <tomocy.dev@gmail.com>
2020-05-24 18:42:03 +0900

    add AuthenticateUser

--- src/usecase/mod.rs
+++ src/usecase/mod.rs
@@ -18,6 +18,24 @@ impl<'a> CreateUser<'a> {
     }
 }
 
+pub struct AuthenticateUser<'a> {
+    repo: &'a Box<dyn super::UserRepo>,
+}
+
+impl<'a> AuthenticateUser<'a> {
+    pub fn new(repo: &'a Box<dyn super::UserRepo>) -> AuthenticateUser {
+        AuthenticateUser { repo }
+    }
+
+    pub fn invoke(&self, email: &str, password: &str) -> Result<Option<super::User>, String> {
+        let user = self.repo.find_by_email(email)?;
+        match user {
+            Some(user) if user.password().verify(password)? => Ok(Some(user)),
+            _ => Ok(None),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::infra::memory;
@@ -37,5 +55,21 @@ mod tests {
             .password()
             .verify(password)
             .expect("should have verified password"));
+    }
+
+    #[test]
+    fn authenticate_user() {
+        let mut repo: Box<dyn UserRepo> = Box::new(memory::UserRepo::new());
+
+        let (email, password) = ("test@example.com", "aiueo");
+        let created = CreateUser::new(&mut repo).invoke(email, password).unwrap();
+
+        let user = AuthenticateUser::new(&repo)
+            .invoke(email, password)
+            .expect("should have succeeded to authenticate user")
+            .expect("should have authenticated user");
+
+        assert_eq!(created.id(), user.id());
+        assert_eq!(created.email(), user.email());
     }
 }
