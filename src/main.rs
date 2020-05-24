10e37ff2417902b3a21264779fd0bc15d6e9189a
tomocy <tomocy.dev@gmail.com>
2020-05-24 18:10:02 +0900

    modify entrypoint
    controller::UserRenderer is injected into cli::App.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate todo;
 
 use std::process;
+use todo::gateway::controller;
 use todo::gateway::controller::cli;
+use todo::gateway::presenter::text;
 use todo::infra::memory;
 
 fn main() {
     let mut repo: Box<dyn todo::UserRepo> = Box::new(memory::UserRepo::new());
-    let mut app = cli::App::new(&mut repo);
+    let renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
+    let mut app = cli::App::new(&mut repo, &renderer);
     if let Err(err) = app.run() {
         eprintln!("{}", err);
         process::exit(1);
