b7e7c186f01d1b1a61d9855f4a6d85a4ae1c9df2
tomocy <tomocy.dev@gmail.com>
2020-05-24 17:57:59 +0900

    update entrypoint

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ extern crate todo;
 
 use std::process;
 use todo::gateway::controller::cli;
+use todo::infra::memory;
 
 fn main() {
-    let app = cli::App {};
+    let mut repo: Box<dyn todo::UserRepo> = Box::new(memory::UserRepo::new());
+    let mut app = cli::App::new(&mut repo);
     if let Err(err) = app.run() {
         eprintln!("{}", err);
         process::exit(1);
