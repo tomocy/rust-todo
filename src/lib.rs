132f9c1e1292ea65c3f5a81c7aeccbdc2b8d39fc
tomocy <tomocy.dev@gmail.com>
2020-05-24 20:42:17 +0900

    update Task
    Task has its name.

--- src/lib.rs
+++ src/lib.rs
@@ -75,16 +75,19 @@ impl Hash {
 pub struct Task {
     id: String,
     user_id: String,
+    name: String,
 }
 
 impl Task {
-    pub fn new(id: &str, user_id: &str) -> Result<Self, String> {
+    pub fn new(id: &str, user_id: &str, name: &str) -> Result<Self, String> {
         Self::verify_id(id)?;
         Self::verify_user_id(user_id)?;
+        Self::verify_name(name)?;
 
         Ok(Task {
             id: id.to_string(),
             user_id: user_id.to_string(),
+            name: name.to_string(),
         })
     }
 
@@ -96,6 +99,10 @@ impl Task {
         &self.user_id
     }
 
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
     fn verify_id(id: &str) -> Result<(), String> {
         verify_not_empty(id).map_err(|_| "id should not be empty")?;
 
@@ -104,6 +111,12 @@ impl Task {
 
     fn verify_user_id(user_id: &str) -> Result<(), String> {
         verify_not_empty(user_id).map_err(|_| "user id should not be empty")?;
+
+        Ok(())
+    }
+
+    fn verify_name(name: &str) -> Result<(), String> {
+        verify_not_empty(name).map_err(|_| "name id should not be empty")?;
 
         Ok(())
     }
