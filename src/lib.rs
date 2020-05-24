0e6660ccde4ee39a8d7674dad2701e21d199f428
tomocy <tomocy.dev@gmail.com>
2020-05-24 14:17:13 +0900

    update User
    User has its email.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 pub struct User {
     id: String,
+    email: String,
 }
 
 impl User {
-    pub fn new(id: &str) -> Result<User, String> {
+    pub fn new(id: &str, email: &str) -> Result<User, String> {
         Self::verify_id(id)?;
+        Self::verify_email(email)?;
 
         Ok(User {
             id: String::from(id),
+            email: String::from(email),
         })
     }
 
@@ -15,9 +18,20 @@ impl User {
         &self.id
     }
 
+    pub fn email(&self) -> &String {
+        &self.email
+    }
+
     fn verify_id(id: &str) -> Result<(), String> {
         match id {
             "" => Err(String::from("id should not be empty")),
+            _ => Ok(()),
+        }
+    }
+
+    fn verify_email(email: &str) -> Result<(), String> {
+        match email {
+            "" => Err(String::from("email should not be empty")),
             _ => Ok(()),
         }
     }
