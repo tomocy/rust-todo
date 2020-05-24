58f0d4e778997562e3a79d08d76b4640150d3b8c
tomocy <tomocy.dev@gmail.com>
2020-05-24 17:57:52 +0900

    update cli::App
    App can create users with the given email and password.

--- src/gateway.rs
+++ src/gateway.rs
@@ -1,10 +1,63 @@
 pub mod controller {
     pub mod cli {
-        pub struct App {}
+        extern crate clap;
 
-        impl App {
-            pub fn run(&self) -> Result<(), String> {
-                Err("not implemented".to_string())
+        use super::super::super::usecase;
+        use super::super::super::UserRepo;
+
+        pub struct App<'a> {
+            user_repo: &'a mut Box<dyn UserRepo>,
+        }
+
+        impl<'a> App<'a> {
+            pub fn new(user_repo: &'a mut Box<dyn UserRepo>) -> App<'a> {
+                App { user_repo }
+            }
+            pub fn run(&mut self) -> Result<(), String> {
+                let args = self.app().get_matches();
+                match args.subcommand() {
+                    ("user", Some(args)) => self.run_user_command(args),
+                    _ => Err("unknown command".to_string()),
+                }
+            }
+
+            fn run_user_command(&mut self, args: &clap::ArgMatches) -> Result<(), String> {
+                match args.subcommand() {
+                    ("create", Some(args)) => {
+                        let (email, password) = (
+                            args.value_of("email").unwrap(),
+                            args.value_of("password").unwrap(),
+                        );
+                        let user = usecase::CreateUser::new(self.user_repo)
+                            .invoke(email, password)
+                            .map_err(|err| format!("failed to create user: {}", err));
+
+                        Ok(())
+                    }
+                    _ => Err("unknown command".to_string()),
+                }
+            }
+
+            fn app<'b, 'c>(&self) -> clap::App<'b, 'c> {
+                clap::App::new("todo").subcommands(vec![self.user_command()])
+            }
+
+            fn user_command<'b, 'c>(&self) -> clap::App<'b, 'c> {
+                clap::SubCommand::with_name("user").subcommands(vec![clap::SubCommand::with_name(
+                    "create",
+                )
+                .arg(
+                    clap::Arg::with_name("email")
+                        .required(true)
+                        .long("email")
+                        .takes_value(true),
+                )
+                .arg(
+                    clap::Arg::with_name("password")
+                        .required(true)
+                        .long("password")
+                        .takes_value(true),
+                )])
             }
         }
     }
