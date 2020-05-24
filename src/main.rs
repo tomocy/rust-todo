64309bcbd3efc9b2113973fc7eba5d4a5a2c4a23
tomocy <tomocy.dev@gmail.com>
2020-05-24 23:06:37 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use todo::gateway::presenter::text;
 use todo::infra::file;
 
 fn main() {
-    let mut repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new("./"));
+    let mut repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new("./").unwrap());
     let renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
     let mut app = cli::App::new(&mut repo, &renderer);
     if let Err(err) = app.run() {
