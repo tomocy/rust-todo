8c760e1ff21f1fbe737657f0173f40b9f95e9d17
tomocy <tomocy.dev@gmail.com>
2020-05-24 18:11:34 +0900

    add controller::Renderer

--- src/gateway/controller/mod.rs
+++ src/gateway/controller/mod.rs
@@ -1,7 +1,12 @@
 use super::super::User;
 
-pub trait UserRenderer {
+pub trait UserRenderer: Renderer {
     fn render_user(&self, user: &User);
+}
+
+pub trait Renderer {
+    fn render_message(&self, msg: &str);
+    fn render_error(&self, msg: &str);
 }
 
 pub mod cli;
