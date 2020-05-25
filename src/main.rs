b4a44b3ec204ca97441abacb1a6980d177d168fe
tomocy <tomocy.dev@gmail.com>
2020-05-25 13:44:24 +0900

    update entrypoint
    text::TaskRenderer is injected into cli::App.

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,16 @@ fn main() {
     let workspace = "./";
     let mut user_repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new(&workspace).unwrap());
     let mut task_repo: Box<dyn todo::TaskRepo> = Box::new(memory::TaskRepo::new());
-    let renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
+    let user_renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
+    let task_renderer: Box<dyn controller::TaskRenderer> = Box::new(text::Text {});
     let mut session_manager: Box<dyn controller::SessionManager> =
         Box::new(file::SessionManager::new(&workspace).unwrap());
 
     let mut app = cli::App::new(
         &mut user_repo,
         &mut task_repo,
-        &renderer,
+        &user_renderer,
+        &task_renderer,
         &mut session_manager,
     );
     if let Err(err) = app.run() {
