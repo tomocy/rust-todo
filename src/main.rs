cac65d111ec878dcaa84bc5ba8e9e309bd36d19a
tomocy <tomocy.dev@gmail.com>
2020-05-25 12:32:04 +0900

    update entrypoint
    file::SessionManager is injected into cli::App.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ use todo::gateway::presenter::text;
 use todo::infra::file;
 
 fn main() {
-    let mut repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new("./").unwrap());
+    let workspace = "./";
+    let mut repo: Box<dyn todo::UserRepo> = Box::new(file::UserRepo::new(&workspace).unwrap());
     let renderer: Box<dyn controller::UserRenderer> = Box::new(text::Text {});
-    let mut app = cli::App::new(&mut repo, &renderer);
+    let mut session_manager: Box<dyn controller::SessionManager> =
+        Box::new(file::SessionManager::new(&workspace).unwrap());
+
+    let mut app = cli::App::new(&mut repo, &renderer, &mut session_manager);
     if let Err(err) = app.run() {
         eprintln!("{}", err);
         process::exit(1);
